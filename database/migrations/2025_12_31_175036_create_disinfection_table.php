642be52c9bd648e77a7608ca635b29a94f5f4b20
elYugen <leo.langlois123@gmail.com>
2026-01-02 00:01:43 +0100

    correction seeder et migration

--- database/migrations/2025_12_31_175036_create_disinfection_table.php
+++ database/migrations/2025_12_31_175036_create_disinfection_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,7 +13,7 @@ return new class extends Migration
         Schema::create('disinfections', function (Blueprint $table) {
             $table->id();
             $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
-            $table->foreignId('vehicle_id')->constrained('vehicle')->onDelete('cascade');
+            $table->foreignId('vehicle_id')->constrained('vehicles')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->date('disinfected_at');
             $table->enum('type', ['daily', 'weekly', 'deep'])->default('daily');
